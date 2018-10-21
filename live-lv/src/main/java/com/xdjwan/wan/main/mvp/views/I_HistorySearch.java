bce009085219cd1db1e218398fe0007077a3f27b
xdj <412806748@qq.com>
2018-10-21 16:35:22 +0800

    111

---
+++ live-lv/src/main/java/com/xdjwan/wan/main/mvp/views/I_HistorySearch.java
@@ -0,0 +1,9 @@
+package com.xdjwan.wan.main.mvp.views;
+
+import com.xdjcore.core.MvpBase.BaseViewInterface;
+
+public interface I_HistorySearch extends BaseViewInterface {
+    void getHistroyData(String json);
+
+
+}
